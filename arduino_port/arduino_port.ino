0e74e564ee9d26be5c9d2baa49753a81c7fbece9
PreetomBoro <gitpreetomboro@gmail.com>
2025-03-27 14:44:25 +0530

    well well well

--- arduino_port/arduino_port.ino
+++ arduino_port/arduino_port.ino
@@ -1,5 +1,5 @@
 // Receiver channels
-#define CH4 A2  // Left/Right
+#define CH1 A2  // Left/Right
 #define CH3 A1  // Brake when lowered
 #define CH2 A0  // Forward/Backward
 
@@ -16,10 +16,13 @@
 #define IN3 8   // Motor B direction
 #define IN4 9
 
+//----------------------------
+#define sensitivity 0.5f
+
 void setup(){
   Serial.begin(115200);
 
-  pinMode(CH4, INPUT);
+  pinMode(CH1, INPUT);
   pinMode(CH2, INPUT);
   pinMode(CH3, INPUT);
 
@@ -42,49 +45,31 @@ int readPWMChannel(int channelInput, int minOutput, int maxOutput, int defaultVa
   int ch = pulseIn(channelInput, HIGH, 40000); // Extended timeout
   if (ch < 100 || ch > 2500) return defaultValue; // Filter invalid signals
   int mappedValue = map(ch, 986, 1978, minOutput, maxOutput);
-  return (abs(mappedValue) < 10) ? 0 : mappedValue;  // Apply dead zone padding of -10 to +10
+  return (abs(mappedValue) < 4) ? 0 : mappedValue;  // Apply dead zone padding of -10 to +10
 }
 
-void moveForward(int speed) {
-  analogWrite(ENA, speed);
-  analogWrite(ENB, speed);
-
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-
+void motorA(int speed){
+  if(speed >0 ){
+    analogWrite(ENA, speed*2.55);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  }else{
+    analogWrite(ENA, -speed*2.55);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  }
 }
 
-void moveBackward(int speed) {
-
-  analogWrite(ENA, speed);
-  analogWrite(ENB, speed);
-
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-
-}
-
-void turnLeft(int speed) {
-  analogWrite(ENA, speed);
-  analogWrite(ENB, speed);
-  digitalWrite(IN1, LOW);  
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
-
-void turnRight(int speed) {
-  analogWrite(ENA, speed);
-  analogWrite(ENB, speed);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+void motorB(int speed){
+  if(speed >0 ){
+    analogWrite(ENB, speed*2.55);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }else{
+    analogWrite(ENB, -speed*2.55);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  }
 }
 
 void applyBrakes() {
@@ -104,12 +89,10 @@ void stopMotors() {
 void test_motors(){
 
   Serial.println("1");
-  moveForward(50);
   delay(1000);
 
 
   Serial.println("200");
-  moveBackward(255);
   
   delay(1000);
   Serial.println("breeh");
@@ -124,68 +107,91 @@ void test_motors(){
 }
 
 //#define debug
-#define testing
+//#define testing
 void loop(){
-  int ch3 = readPWMChannel(CH3, 0, 100, 0);
+  //----------------------------------------------------------------------
+  #ifdef debug
+  int ch1 = readPWMChannel(CH1, -100, 100, 0);
+  
+  Serial.println("Debuging\n");
+  Serial.println(ch1);
+  moveForward(255);
+  
+  #endif
 
   //---------------------------------------------------------------------------
   #ifndef debug
+
+  int ch3 = readPWMChannel(CH3, 0, 100, 0);
   if (ch3 == 0) { // Brake condition
     Serial.println("Applying brakes!");
     applyBrakes();
     return;  // Skip other controls while braking
   }
+  
+  int speedA =0;
+  int speedB =0;
 
   // Priority: Left/Right first
   //proper priority
-  int ch4 = readPWMChannel(CH4, -100, 100, 0);
-  digitalWrite(LEYE, HIGH);
-  if(ch4 > 0) {
-    digitalWrite(LEYE, LOW);
-    #ifdef testing
-    Serial.print("Turning right with speed: ");
-    Serial.println(ch4);
-    #endif
-    turnRight(ch4*(2.55*ch3/100));
-  }  
-  digitalWrite(REYE, HIGH);
-  if(ch4 < 0){
-    digitalWrite(REYE, LOW);
-    #ifdef testing
-    Serial.print("Turning left with speed: ");
-    Serial.println(ch4*(2.55*ch3/100));
-    #endif
-    ch4 *=-1;
-    turnLeft(ch4*2.55);
-  }
-
+  int ch1 = readPWMChannel(CH1, -100, 100, 0);
   int ch2 = readPWMChannel(CH2, -100, 100, 0);
+  
+  digitalWrite(REYE, HIGH);
+  digitalWrite(LEYE, HIGH);
+
+  //added nesting for proper PRIORITY (I am looking at you Jaikhlong)
+  if(ch1 > 0) {
+    digitalWrite(LEYE, LOW);
+    //turnRight(2.55*ch3); can't do mixing with this approach
+    speedA -= ch3;
+    speedB += ch3;
+    
+    if(ch2 > 0){
+      speedA += sensitivity*ch2;
+    }else if(ch2 < 0){
+      speedB -= sensitivity*ch2;
+    }
+    
+    motorA(speedA);
+    motorB(speedB);
+    return;
+  }
+  else if(ch1 < 0){
+    digitalWrite(REYE, LOW);
+    //turnLeft(2.55*ch3); can't do mixing & 3rd channel sensitivity, for jack sht, with the dummified procedural style (ohh please reffer to the 69 functions for doing specific task, hell naw)
+    speedA += ch3;
+    speedB -= ch3;
+    
+    if(ch2 > 0){
+      speedB += sensitivity*ch2;
+    }else if(ch2 < 0){
+      speedA -= sensitivity*ch2;
+    }
+    motorA(speedA);
+    motorB(speedB);
+    return;
+  }
   if(ch2 > 0){
-    moveForward(ch2*(2.55*ch3/100));
-    #ifdef testing
-    Serial.print("Moving forward with speed: ");
-    Serial.println(-ch2);
-    #endif
+    speedA += ch3;
+    speedB += ch3;
+  }else if(ch2 < 0){
+    speedA -= ch3;
+    speedB -= ch3;
   }
-  if(ch2 < 0) {
-    ch2 *= -1;
-    moveBackward(ch2*(2.55*ch3/100));
-    #ifdef testing
-    Serial.print("Moving backward with speed: ");
-    Serial.println(ch2);
-    #endif
+  motorA(speedA);
+  motorB(speedB);
+  
+  #ifdef testing
+  Serial.print("blah blah blah, bluh bluh bluh ");
+  Serial.println(speedA);
+  Serial.println(speedB);
+  #endif
+  
+  if(ch2 == 0 && ch1 == 0) {
+    stopMotors();
   }
-  if(ch2 == 0 && ch4 == 0) stopMotors();
 
   #endif 
  
-  //----------------------------------------------------------------------
-  #ifdef debug
-  int ch4 = readPWMChannel(CH4, -100, 100, 0);
-  
-  Serial.println("Debuging\n");
-  Serial.println(ch4);
-  moveForward(255);
-  
-  #endif
 }
